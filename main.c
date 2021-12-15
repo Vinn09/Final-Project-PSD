2cf2bea0b38b2d667ad9b6acc08beea62fdb11cb
Vinn09 <84385458+Vinn09@users.noreply.github.com>
2021-12-15 23:45:01 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int pil;
+
+    printf("=============== PROGRAM ANTRIAN RUMAH MAKAN ===============\n\n");
+    printf("Pilihan Menu : \n");
+    printf("   1. Tambah Antrian\n");
+    printf("   2. Lihat Antrian\n");
+    printf("   3. Pembayaran\n");
+    printf("   4. Kosongkan Antrian\n");
+    printf("   5. Tambah Menu\n");
+    printf("   6. Exit\n");
+    printf("Ketik Pilihan : ");
+    scanf("%d", &pil);
+
+    switch(pil)
+    {
+    case 1:
+
+    }
+    return 0;
+}
